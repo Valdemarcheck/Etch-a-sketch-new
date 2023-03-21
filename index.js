39c18b427828a406a59d5d4e835f71ed73998be5
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-03-21 15:34:04 +0500

    Add fillGrid function

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ const colorPicker = document.querySelector(".color-btn");
 const randomColorTool = document.querySelector(".random-color-btn");
 
 const canvas = document.querySelector(".canvas");
+
+let pixels = [];
+let brushSize = 1;
+let currentColor = '#000000';
+
+function fillGrid(width) {
+    for (let i = 0; i < width; i++) {
+        pixels.push([]);
+        for (let j = 0; j < width; j++) {
+          let pixel = document.createElement("div");
+
+          pixel.style.border = "1px solid black";
+          pixel.style.minHeight = "30px";
+          pixel.style.minWidth = "30px";
+          pixel.setAttribute("index", `${i},${j}`);
+
+          pixels[i].push(pixel);
+          canvas.appendChild(pixel);
+        }
+      }
+}
+
+fillGrid(16);
