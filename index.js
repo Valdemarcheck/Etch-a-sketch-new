e0249506ba37f7b47b769548ba38405fe0fc6949
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-03-21 15:31:17 +0500

    Get required references

---
+++ index.js
@@ -0,0 +1,10 @@
+// Get all required elements references
+
+const penTool = document.querySelector(".pen-btn");
+const eraserTool = document.querySelector(".eraser-btn");
+const clearTool = document.querySelector(".clear-btn");
+const penSizeTool = document.querySelector(".pen-size-btn");
+const colorPicker = document.querySelector(".color-btn");
+const randomColorTool = document.querySelector(".random-color-btn");
+
+const canvas = document.querySelector(".canvas");
